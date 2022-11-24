49083df1dd8370b2454459665616c5ea363fe3bc
“Daniel <daniel.schoenfeldt@gmx.de>
2022-11-24 11:44:22 +0100

    dynamodbkutter.tf changed ressouce name to dynamodb-kutter-table

--- infrastructure/dynamodbkutter.tf
+++ infrastructure/dynamodbkutter.tf
@@ -1,4 +1,4 @@
-resource "aws_dynamodb_table" "basic-dynamodb-table" {
+resource "aws_dynamodb_table" "dynamodb-kutter-table" {
   name     = "kutter-table"
   billing_mode   = "PROVISIONED"
   read_capacity  = 5
