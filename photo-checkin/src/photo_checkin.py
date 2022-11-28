d326527ef9e124a48ffd28ec36a5a05c7a8ed6fa
“Daniel <daniel.schoenfeldt@gmx.de>
2022-11-28 17:17:20 +0100

    lambda: photo_checkin.py added code for put_db_object()

--- photo-checkin/src/photo_checkin.py
+++ photo-checkin/src/photo_checkin.py
@@ -5,15 +5,24 @@ def get_s3_object(event):
     bucket = event['Records'][0]['s3']['bucket']['name']
     file_name = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
     creation_date = event['Records'][0]['eventTime']
-    return bucket, file_name, creation_date
+    return file_name, bucket, creation_date
 
-def create_db_object_id():
-    object_id = ()
+def generate_db_object_id():
+    object_id = test123
     return object_id
 
-def put_db_object(bucket, file_name, creation_date, object_id):
+def put_db_object(object_id, file_name, bucket, creation_date):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('kutter-table')
+    response = table.put_item(
+        Item={
+           "objectId": object_id,
+           "originalFileName": file_name,
+           "s3Bucket": bucket,
+           "creationDate": creation_date,
+   })
 
 def handler(event, context):     
-    bucket, file_name, creation_date = get_s3_object(event)
-    object_id = create_db_object_id()
-    put_db_object(bucket, file_name, creation_date, object_id)
+    file_name, bucket, creation_date = get_s3_object(event)
+    object_id = generate_db_object_id()
+    put_db_object(object_id, file_name, bucket, creation_date)
