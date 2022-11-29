a8b77ea81461ddca0a49c65f73310c50f1d5fce1
“Daniel <daniel.schoenfeldt@gmx.de>
2022-11-29 13:21:39 +0100

    changed iam policy

--- infrastructure/lambda_photo_checkin.tf
+++ infrastructure/lambda_photo_checkin.tf
@@ -9,7 +9,7 @@ resource "aws_lambda_permission" "allow_bucket" {
 resource "aws_lambda_function" "photo_checkin" {
   function_name = "photo_checkin"
   filename      = "build/photo_checkin.zip"
-  role          = aws_iam_role.nfish-des-role-lambda_s3_access.arn
+  role          = aws_iam_role.nfish-des-role-lambda_s3_dynamodb.arn
   handler       = "photo_checkin.handler"
   timeout       = 300
   runtime       = "python3.9"
