fa9b92feaecf16535b24b293e56fd7e540ebb20a
“Daniel <daniel.schoenfeldt@gmx.de>
2022-11-30 14:52:34 +0100

    small correction of bucket names, not important

--- infrastructure/scripts/setup_terraform_bucket.sh
+++ infrastructure/scripts/setup_terraform_bucket.sh
@@ -1,4 +1,4 @@
-S3_BUCKET_NAME=nfish-des-kutter-terraform-state1
+S3_BUCKET_NAME=nfish-des-kutter-terraform-state
 REGION=us-west-2
 if aws s3 ls "s3://$S3_BUCKET_NAME" 2>&1 | grep -q 'An error occurred'
 then
