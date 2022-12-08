ebbe3ef7195bdfa4117efe730e704c5a9fc4ef79
“Daniel <daniel.schoenfeldt@gmx.de>
2022-12-08 10:00:32 +0100

    added lambda with api gateway trigger api_gateway.tf with policy

--- infrastructure/scripts/make-zip-photo-checkin-src.sh
+++ infrastructure/scripts/make-zip-photo-checkin-src.sh
@@ -1,13 +1,10 @@
 # make zip for lambda function photo_checkin.py
 mkdir ../../infrastructure/build
 
-cd ../../photo-checkin/src
+cd ../../photo_checkin/src
 zip -r ../../infrastructure/build/photo_checkin.zip .
-cd ../..
 
 # make zip for lambda function get_objects.py
-mkdir ../../infrastructure/build
-
 cd ../../get_objects/src
 zip -r ../../infrastructure/build/get_objects.zip .
 cd ../..
