b6e8573bf69768b1e6b490bfae8728e04610402c
“Daniel <daniel.schoenfeldt@gmx.de>
2022-12-09 16:14:33 +0100

    added search_objects function to api_gateway.tf

--- infrastructure/api_gateway.tf
+++ infrastructure/api_gateway.tf
@@ -3,6 +3,8 @@ resource "aws_api_gateway_rest_api" "kutter-API" {
   description = "for AWS Lambda trigger"
 }
 
+# get-object endpoint:
+
 resource "aws_api_gateway_resource" "get-object" {
   rest_api_id = "${aws_api_gateway_rest_api.kutter-API.id}"
   parent_id   = "${aws_api_gateway_rest_api.kutter-API.root_resource_id}"
@@ -25,6 +27,33 @@ resource "aws_api_gateway_integration" "lambda" {
   type                    = "AWS_PROXY"
   uri                     = "${aws_lambda_function.get_object.invoke_arn}"
 }
+
+# search-objects endpoint:
+
+resource "aws_api_gateway_resource" "search-objects" {
+  rest_api_id = "${aws_api_gateway_rest_api.kutter-API.id}"
+  parent_id   = "${aws_api_gateway_rest_api.kutter-API.root_resource_id}"
+  path_part   = "search-objects"
+}
+
+resource "aws_api_gateway_method" "search-objects" {
+  rest_api_id   = "${aws_api_gateway_rest_api.kutter-API.id}"
+  resource_id   = "${aws_api_gateway_resource.search-objects.id}"
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda" {
+  rest_api_id = "${aws_api_gateway_rest_api.kutter-API.id}"
+  resource_id = "${aws_api_gateway_method.search-objects.resource_id}"
+  http_method = "${aws_api_gateway_method.search-objects.http_method}"
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "${aws_lambda_function.search_objects.invoke_arn}"
+}
+
+# list-objects endpoint (root):
 
 resource "aws_api_gateway_method" "proxy_root" {
   rest_api_id   = "${aws_api_gateway_rest_api.kutter-API.id}"
