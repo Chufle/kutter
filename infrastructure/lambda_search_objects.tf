e57e190d827e9cfe655cdc35388d57cccd941f9c
“Daniel <daniel.schoenfeldt@gmx.de>
2022-12-09 16:57:54 +0100

    api endpoint ?search= working

--- infrastructure/lambda_search_objects.tf
+++ infrastructure/lambda_search_objects.tf
@@ -6,7 +6,7 @@ resource "aws_lambda_permission" "apigw_search_objects" {
 
   # The /*/* portion grants access from any method on any resource
   # within the API Gateway "REST API".
-  source_arn = "${aws_api_gateway_rest_api.kutter-API.execution_arn}/*/*"
+  source_arn = "${aws_api_gateway_rest_api.kutter_API.execution_arn}/*/*"
 }
 
 resource "aws_lambda_function" "search_objects" {
--- search_objects/src/search_objects.py
+++ search_objects/src/search_objects.py
@@ -1,27 +1,21 @@
 import json
 import boto3
 from boto3.dynamodb.conditions import Attr, Or
-from typing import Union
 
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('kutter-table')
 
-def search_objects_data(search, exclusiveStartKey, limit):
+def search_objects_data(search, limit):
     filter_expression = Or( Attr('creationDate').contains(search), Attr('originalFileName').contains(search))
-    response = table.scan(
-        Limit = limit,  
-        FilterExpression = filter_expression,
-        ExclusiveStartKey = {"objectId":exclusiveStartKey}) if  exclusiveStartKey is not None else table.scan(Limit = limit,FilterExpression = filter_expression) 
+    response = table.scan(Limit = limit,FilterExpression = filter_expression) 
     return { 
-         'items' :response['Items'],
-         'LastEvaluatedKey': response['LastEvaluatedKey']
+         'items' :response['Items']
     }
 
 def handler(event, context):
     search = event ['queryStringParameters']['search']
-    exclusiveStartKey: Union[str, None] = None
-    limit: int = 10
-    items = search_objects_data(search,exclusiveStartKey,limit)
+    limit = int(10)
+    items = search_objects_data(search,limit)
     return {
         'statusCode': 200,
         'headers': {
