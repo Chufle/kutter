7e411eeedb5cbedacbfa7af1e395cdd9c155859a
“Daniel <daniel.schoenfeldt@gmx.de>
2022-12-12 18:07:39 +0100

    new lambda and API for news_crawler.py

--- infrastructure/api_gateway.tf
+++ infrastructure/api_gateway.tf
@@ -53,6 +53,31 @@ resource "aws_api_gateway_integration" "lambda_search_objects" {
   uri                     = "${aws_lambda_function.search_objects.invoke_arn}"
 }
 
+# news-crawler endpoint:
+
+resource "aws_api_gateway_resource" "news_crawler" {
+  rest_api_id = "${aws_api_gateway_rest_api.kutter_API.id}"
+  parent_id   = "${aws_api_gateway_rest_api.kutter_API.root_resource_id}"
+  path_part   = "news-crawler"
+}
+
+resource "aws_api_gateway_method" "news_crawler" {
+  rest_api_id   = "${aws_api_gateway_rest_api.kutter_API.id}"
+  resource_id   = "${aws_api_gateway_resource.news_crawler.id}"
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda_news_crawler" {
+  rest_api_id = "${aws_api_gateway_rest_api.kutter_API.id}"
+  resource_id = "${aws_api_gateway_method.news_crawler.resource_id}"
+  http_method = "${aws_api_gateway_method.news_crawler.http_method}"
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "${aws_lambda_function.news_crawler.invoke_arn}"
+}
+
 # list-objects endpoint (root):
 
 resource "aws_api_gateway_method" "proxy_root" {
@@ -76,6 +101,7 @@ resource "aws_api_gateway_deployment" "kutter" {
   depends_on = [
     aws_api_gateway_integration.lambda_get_object,
     aws_api_gateway_integration.lambda_search_objects,
+    aws_api_gateway_integration.lambda_news_crawler,
     aws_api_gateway_integration.lambda_root,
   ]
 
--- infrastructure/lambda_news_crawler.tf
+++ infrastructure/lambda_news_crawler.tf
@@ -1,3 +1,14 @@
+resource "aws_lambda_permission" "apigw_news_crawler" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.news_crawler.function_name}"
+  principal     = "apigateway.amazonaws.com"
+
+  # The /*/* portion grants access from any method on any resource
+  # within the API Gateway "REST API".
+  source_arn = "${aws_api_gateway_rest_api.kutter_API.execution_arn}/*/*"
+}
+
 resource "aws_lambda_function" "news_crawler" {
   function_name = "news_crawler"
   filename      = "build/news_crawler.zip"
